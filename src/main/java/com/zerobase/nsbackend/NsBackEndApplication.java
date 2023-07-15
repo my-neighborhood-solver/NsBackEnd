8fbf491b4e3380e0320489fe51e7f0a7529a1a63
gyureal <yonggyu.jeong10@gmail.com>
2023-07-15 18:23:32 +0900

    init: init project

---
+++ src/main/java/com/zerobase/nsbackend/NsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.nsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsBackEndApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NsBackEndApplication.class, args);
+  }
+
+}
