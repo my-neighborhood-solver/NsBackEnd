8bdd94efe8c546aace0ed853634d88ec22891523
KIM <wltn9169@naver.com>
2023-07-23 03:10:53 +0900

    채팅방,채팅내용 엔티티 #10

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
-#docker run --name neighborSolver-dev -e MYSQL_ROOT_PASSWORD=12 -e MYSQL_DATABASE=neighborSolver -d -p 3376:3306 mysql
-#docker run --name neighborSolver-test -e MYSQL_ROOT_PASSWORD=12 -e MYSQL_DATABASE=neighborSolver -d -p 3386:3306 mysql
+docker run --name neighborSolver-dev -e MYSQL_ROOT_PASSWORD=12 -e MYSQL_DATABASE=neighborSolver -d -p 3376:3306 mysql
+docker run --name neighborSolver-test -e MYSQL_ROOT_PASSWORD=12 -e MYSQL_DATABASE=neighborSolver -d -p 3386:3306 mysql
 
