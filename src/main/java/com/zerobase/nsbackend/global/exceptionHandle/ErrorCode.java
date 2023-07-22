4725917ca543b75ce64bd1135bbc5db4abd84955
gyureal <yonggyu.jeong10@gmail.com>
2023-07-22 19:37:25 +0900

    feat(global): 공통 예외처리 구현
    resolved: #15

---
+++ src/main/java/com/zerobase/nsbackend/global/exceptionHandle/ErrorCode.java
@@ -0,0 +1,26 @@
+package com.zerobase.nsbackend.global.exceptionHandle;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+  MEMBER_NOT_FOUND("10001", "회원을 찾지 못했습니다.");
+  private final String code;
+  private final String description;
+
+  private static final Map<String, ErrorCode> ERROR_CODE_MAP = new HashMap<>();
+
+  static {
+    for (ErrorCode errorCode : ErrorCode.values()) {
+      ERROR_CODE_MAP.put(errorCode.description, errorCode);
+    }
+  }
+
+  public static ErrorCode findCode(String description) {
+    return ERROR_CODE_MAP.get(description);
+  }
+}
