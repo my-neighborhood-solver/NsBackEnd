abd08adabd806e90f4d45427282c84792d249ad6
gyureal <yonggyu.jeong10@gmail.com>
2023-07-22 19:37:25 +0900

    feat(global): 공통 예외처리 구현
    resolved: #15

---
+++ src/main/java/com/zerobase/nsbackend/global/exceptionHandle/ErrorResponse.java
@@ -0,0 +1,29 @@
+package com.zerobase.nsbackend.global.exceptionHandle;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ErrorResponse {
+  private final String errorCode;
+  private final String description;
+  private final LocalDateTime dateTime;
+  private final UUID logId;
+
+  public static ErrorResponse of(UUID logId, Exception exception) {
+    ErrorCode errorcode = ErrorCode.findCode(exception.getMessage());
+
+    return ErrorResponse.builder()
+        .errorCode(errorcode.getCode())
+        .description(errorcode.getDescription())
+        .dateTime(LocalDateTime.now())
+        .logId(logId)
+        .build();
+  }
+}
