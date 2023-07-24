c09a8ce6ecfbc1894a397282d6f923e59049b8a0
SeokCheol <lsc991123@gmail.com>
2023-07-24 16:22:29 +0900

    feat : 소셜로그인(카카오) 회원가입 구현

--- src/main/java/com/zerobase/nsbackend/member/web/AuthController.java
+++ src/main/java/com/zerobase/nsbackend/member/web/AuthController.java
@@ -1,12 +1,16 @@
 package com.zerobase.nsbackend.member.web;
 
 import com.zerobase.nsbackend.member.domain.Member;
-import com.zerobase.nsbackend.member.security.TokenProvider;
-import com.zerobase.nsbackend.member.service.AuthService;
 import com.zerobase.nsbackend.member.dto.Auth.SignIn;
 import com.zerobase.nsbackend.member.dto.Auth.SignInResponse;
 import com.zerobase.nsbackend.member.dto.Auth.SignUp;
 import com.zerobase.nsbackend.member.dto.Auth.SignUpResponse;
+import com.zerobase.nsbackend.member.dto.KakaoTokenResponse;
+import com.zerobase.nsbackend.member.dto.KakaoUserInfoResponse;
+import com.zerobase.nsbackend.member.security.TokenProvider;
+import com.zerobase.nsbackend.member.service.AuthService;
+import com.zerobase.nsbackend.member.utils.KakaoTokenJsonData;
+import com.zerobase.nsbackend.member.utils.KakaoUserInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Slf4j
 @RequestMapping("/auth")
@@ -25,6 +31,8 @@ public class AuthController {
 
     private final AuthService authService;
     private final TokenProvider tokenProvider;
+    private final KakaoTokenJsonData kakaoTokenJsonData;
+    private final KakaoUserInfo kakaoUserInfo;
 
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponse> createMember(@RequestBody SignUp request){
@@ -33,7 +41,6 @@ public class AuthController {
             .id(member.getId())
             .email(member.getEmail())
             .name(member.getName())
-            .password(member.getPassword())
             .build();
         return ResponseEntity.ok(response);
     }
@@ -47,6 +54,33 @@ public class AuthController {
             .email(member.getEmail())
             .name(member.getName())
             .token(token)
+            .build();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/social/kakao")
+    public RedirectView kakaoLogin (){
+        RedirectView redirectView = new RedirectView();
+        String url = authService.kakaoLogin();
+        redirectView.setUrl(url);
+        return redirectView;
+    }
+
+    @GetMapping("/kakao/callback")
+    @ResponseBody
+    public ResponseEntity<SignUpResponse> kakaoOauth(@RequestParam("code") String code) {
+        log.info("인가 코드를 이용하여 토큰을 받습니다. : "+code);
+        KakaoTokenResponse kakaoTokenResponse = kakaoTokenJsonData.getToken(code);
+        log.info("토큰에 대한 정보입니다. : {}",kakaoTokenResponse);
+        KakaoUserInfoResponse userInfo = kakaoUserInfo.getUserInfo(kakaoTokenResponse.getAccess_token());
+        log.info("회원 정보 입니다.{}",userInfo);
+        String name = userInfo.getKakao_account().getProfile().getNickname();
+        Member member = this.authService.kakaoRegister(userInfo.getKakao_account().getEmail(),
+            name);
+        SignUpResponse response = SignUpResponse.builder()
+            .id(member.getId())
+            .email(member.getEmail())
+            .name(member.getName())
             .build();
         return ResponseEntity.ok(response);
     }
