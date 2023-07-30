2e8d5e3bd919e5a72097211d91c0cb27acaaa0e5
KIM <wltn9169@naver.com>
2023-07-31 02:59:24 +0900

    채팅방 전체 && 단건 조회

---
+++ src/main/java/com/zerobase/nsbackend/chatting/dto/ChattingRoomAllResponse.java
@@ -0,0 +1,31 @@
+package com.zerobase.nsbackend.chatting.dto;
+
+import com.zerobase.nsbackend.chatting.domain.entitiy.ChattingRoom;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class ChattingRoomAllResponse {
+
+  private Long ChattingRoom_Id;
+  private String ChattingContent;
+  private String nickName;
+  private LocalDateTime time;
+  private Long readCount;
+
+
+
+  public static ChattingRoomAllResponse from(ChattingRoom chattingRoom, String content, LocalDateTime time,Long readCount) {
+    return ChattingRoomAllResponse.builder()
+        .ChattingRoom_Id(chattingRoom.getId())
+        .ChattingContent(content)
+        .nickName(chattingRoom.getSender().getNickname())
+        .time(time)
+        .readCount(readCount)
+        .build();
+  }
+}
--- src/main/java/com/zerobase/nsbackend/chatting/web/ChattingRoomController.java
+++ src/main/java/com/zerobase/nsbackend/chatting/web/ChattingRoomController.java
@@ -1,10 +1,15 @@
 package com.zerobase.nsbackend.chatting.web;
 
+import com.zerobase.nsbackend.chatting.domain.entitiy.ChattingContent;
 import com.zerobase.nsbackend.chatting.domain.service.ChattingRoomService;
+import com.zerobase.nsbackend.chatting.dto.ChattingRoomAllResponse;
 import com.zerobase.nsbackend.chatting.dto.ChattingRoomCreateRequest;
 import com.zerobase.nsbackend.chatting.dto.ChattingRoomCreateResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +25,25 @@ public class ChattingRoomController {
 
   //채팅방 생성
   @PostMapping("")
-  public ResponseEntity<ChattingRoomCreateResponse> createChattingRoom(
-      @RequestBody ChattingRoomCreateRequest request) {
+  public ResponseEntity<ChattingRoomCreateResponse> createChattingRoom(@RequestBody ChattingRoomCreateRequest request) {
 
     //conflict 충돌로 이미 생성되어 있는 채팅방  success 생성된 채팅방
 
     return chattingRoomService
         .createChattingRoom(request.getErrand_Id(), request.getSender_Id());
+  }
+
+  //채팅방 전체조회
+  @GetMapping("/memberId={memberId}")
+  public ResponseEntity<List<ChattingRoomAllResponse>> getChattingRooms(@PathVariable Long memberId) {
+    List<ChattingRoomAllResponse> chattingRooms = chattingRoomService.getChattingRoomsByMemberId(memberId);
+    return ResponseEntity.ok(chattingRooms);
+  }
+
+  // 채팅방 단건 조회
+  @GetMapping("/{roomId}/memberId={memberId}")
+  public ResponseEntity<List<ChattingContent>> getChattingRoom(@PathVariable Long roomId, @PathVariable Long memberId) {
+    List<ChattingContent> chattingRoom = chattingRoomService.getChattingRoomByIdAndMemberId(roomId, memberId);
+    return ResponseEntity.ok().body(chattingRoom);
   }
 }
