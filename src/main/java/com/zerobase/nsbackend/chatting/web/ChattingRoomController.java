a06cc1f16e340ae3a64a20374cfaabfdec336afe
KIM <wltn9169@naver.com>
2023-08-01 22:41:35 +0900

    채팅방 생성 !

---
+++ src/main/java/com/zerobase/nsbackend/chatting/web/ChattingRoomController.java
@@ -0,0 +1,31 @@
+package com.zerobase.nsbackend.chatting.web;
+
+import com.zerobase.nsbackend.chatting.domain.service.ChattingRoomService;
+import com.zerobase.nsbackend.chatting.dto.ChattingRoomCreateRequest;
+import com.zerobase.nsbackend.chatting.dto.ChattingRoomCreateResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chats")
+public class ChattingRoomController {
+
+  private final ChattingRoomService chattingRoomService;
+
+
+  //채팅방 생성
+  @PostMapping("")
+  public ResponseEntity<ChattingRoomCreateResponse> createChattingRoom(
+      @RequestBody ChattingRoomCreateRequest request) {
+
+    //conflict 충돌로 뜨면 이미 생성되어 있는 채팅방  success 생성된 채팅방
+
+    return chattingRoomService
+        .createChattingRoom(request.getErrand_Id(), request.getSender_Id());
+  }
+}
