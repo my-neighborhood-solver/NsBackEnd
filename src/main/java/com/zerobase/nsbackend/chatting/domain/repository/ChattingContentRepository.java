acb32a520e3112f8e2748185b33434fcccb938ea
KIM <wltn9169@naver.com>
2023-08-01 22:41:35 +0900

    채팅방 생성 !

--- src/main/java/com/zerobase/nsbackend/chatting/domain/repository/ChattingContentRepository.java
+++ src/main/java/com/zerobase/nsbackend/chatting/domain/repository/ChattingContentRepository.java
@@ -1,13 +1,12 @@
 package com.zerobase.nsbackend.chatting.domain.repository;
 
-
 import com.zerobase.nsbackend.chatting.domain.entity.ChattingContent;
-
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChattingContentRepository extends JpaRepository<ChattingContent, Long> {
+
 
 }
--- src/main/java/com/zerobase/nsbackend/chatting/domain/repository/ChattingRoomRepository.java
+++ src/main/java/com/zerobase/nsbackend/chatting/domain/repository/ChattingRoomRepository.java
@@ -1,11 +1,17 @@
 package com.zerobase.nsbackend.chatting.domain.repository;
 
+
 import com.zerobase.nsbackend.chatting.domain.entity.ChattingRoom;
+import com.zerobase.nsbackend.errand.domain.Errand;
+import com.zerobase.nsbackend.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChattingRoomRepository extends JpaRepository<ChattingRoom, Long> {
+
+  ChattingRoom findByErrand_MemberAndSender(Errand errand, Member sender);
+
 
 
 }
