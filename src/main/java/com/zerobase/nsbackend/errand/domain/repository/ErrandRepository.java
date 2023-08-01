43710ce7bdd4130a62c0023c2726b8f7856f327f
gyureal <yonggyu.jeong10@gmail.com>
2023-08-01 22:31:15 +0900

    refactor(errand): 의뢰 엔티티 디렉토리 이동 domain -> domain.entity
    related: #36

--- src/main/java/com/zerobase/nsbackend/errand/domain/repository/ErrandRepository.java
+++ src/main/java/com/zerobase/nsbackend/errand/domain/repository/ErrandRepository.java
@@ -1,6 +1,6 @@
 package com.zerobase.nsbackend.errand.domain.repository;
 
-import com.zerobase.nsbackend.errand.domain.Errand;
+import com.zerobase.nsbackend.errand.domain.entity.Errand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ErrandRepository extends JpaRepository<Errand, Long> {
