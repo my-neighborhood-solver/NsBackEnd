7c5b27071f592fdca3fd50c38dc9aef73958a4ba
gyureal <yonggyu.jeong10@gmail.com>
2023-08-01 22:35:13 +0900

    feat(errand): 의뢰 이미지 엔티티 추가
    related: #36

--- src/main/java/com/zerobase/nsbackend/errand/domain/entity/Errand.java
+++ src/main/java/com/zerobase/nsbackend/errand/domain/entity/Errand.java
@@ -4,6 +4,7 @@ import com.zerobase.nsbackend.errand.domain.vo.ErrandStatus;
 import com.zerobase.nsbackend.errand.domain.vo.PayDivision;
 import com.zerobase.nsbackend.global.BaseTimeEntity;
 import com.zerobase.nsbackend.member.domain.Member;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,6 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,6 +35,8 @@ public class Errand extends BaseTimeEntity {
   private String title;
   @Column(columnDefinition = "TEXT")
   private String content;
+  @OneToMany(mappedBy = "errand")
+  private List<ErrandImage> images;
   @Enumerated(EnumType.STRING)
   private PayDivision payDivision;
   private Integer pay;
