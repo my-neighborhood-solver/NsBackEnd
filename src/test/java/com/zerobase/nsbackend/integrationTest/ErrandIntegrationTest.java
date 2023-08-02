69a54e1b6c1a14b01984b2683c9c2a0078984d14
gyureal <yonggyu.jeong10@gmail.com>
2023-08-02 19:19:56 +0900

    test(errand): 의뢰 생성 테스트시, 의뢰 사진 기능 테스트 추가
    related: #36

--- src/test/java/com/zerobase/nsbackend/integrationTest/ErrandIntegrationTest.java
+++ src/test/java/com/zerobase/nsbackend/integrationTest/ErrandIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.zerobase.nsbackend.integrationTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -10,6 +12,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
+import com.zerobase.nsbackend.errand.domain.entity.Errand;
+import com.zerobase.nsbackend.errand.domain.entity.ErrandImage;
+import com.zerobase.nsbackend.errand.domain.repository.ErrandRepository;
 import com.zerobase.nsbackend.errand.domain.vo.PayDivision;
 import com.zerobase.nsbackend.errand.dto.ErrandCreateRequest;
 import com.zerobase.nsbackend.errand.dto.ErrandUpdateRequest;
@@ -17,12 +22,15 @@ import com.zerobase.nsbackend.global.exceptionHandle.ErrorCode;
 import com.zerobase.nsbackend.member.domain.Member;
 import com.zerobase.nsbackend.member.repository.MemberRepository;
 import com.zerobase.nsbackend.member.type.Authority;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
@@ -32,6 +40,8 @@ class ErrandIntegrationTest extends IntegrationTest {
 
   @Autowired
   MemberRepository memberRepository;
+  @Autowired
+  ErrandRepository errandRepository;
 
   Member member01;
   Member member02;
@@ -52,8 +62,8 @@ class ErrandIntegrationTest extends IntegrationTest {
   }
 
   @Test
-  @DisplayName("의뢰 생성에 성공합니다.")
-  void createErrand_success() throws Exception {
+  @DisplayName("의뢰 생성에 성공합니다 (첨부 사진 없는 경우)")
+  void createErrand_success_when_no_images() throws Exception {
     // given
     ErrandCreateRequest createRequest = ErrandCreateRequest.builder()
         .title("testTitle")
@@ -75,6 +85,54 @@ class ErrandIntegrationTest extends IntegrationTest {
     resultActions
         .andExpect(status().isCreated())
         .andExpect(header().string("Location", CoreMatchers.startsWith("/errands/")));
+  }
+
+  @Test
+  @DisplayName("의뢰 생성에 성공합니다 (첨부사진 있는 경우)")
+  void createErrand_success_with_images() throws Exception {
+    // given
+    MockMultipartFile file1 = makeMultipartFile();
+    MockMultipartFile file2 = makeMultipartFile();
+
+    ErrandCreateRequest createRequest = ErrandCreateRequest.builder()
+        .title("testTitle")
+        .content("testContent")
+        .payDivision(PayDivision.HOURLY)
+        .pay(10000)
+        .build();
+
+    MockMultipartFile jsonRequest = new MockMultipartFile("errand", "",
+        "application/json", asJsonString(createRequest).getBytes());
+
+    // when
+    ResultActions resultActions = mvc.perform(
+            multipart("/errands")
+                .file(file1)
+                .file(file2)
+                .file(jsonRequest)
+                .contentType(MediaType.MULTIPART_FORM_DATA)
+                .with(user(member01.getEmail()))
+        )
+        .andDo(print());
+
+    // then
+    MvcResult result = resultActions
+        .andExpect(status().isCreated())
+        .andExpect(header().string("Location", CoreMatchers.startsWith("/errands/")))
+        .andReturn();
+
+    Long newId = parseNewId(result);
+    Errand newErrand = errandRepository.findById(newId)
+        .orElseThrow(() -> new RuntimeException("test fail"));
+    assertThat(newErrand.getTitle()).isEqualTo(createRequest.getTitle());
+    assertThat(newErrand.getContent()).isEqualTo(createRequest.getContent());
+    assertThat(newErrand.getImages()).hasSize(2);
+    assertThat(newErrand.getPayDivision()).isEqualTo(createRequest.getPayDivision());
+    assertThat(newErrand.getPay()).isEqualTo(createRequest.getPay());
+
+    // 테스트로 업로드된 파일 삭제
+    removeFilesAfterTest(newErrand.getImages().stream().map(ErrandImage::getImageUrl)
+        .collect(Collectors.toList()));
   }
 
   @WithMockUser(roles = "USER")
@@ -228,6 +286,10 @@ class ErrandIntegrationTest extends IntegrationTest {
     MvcResult mvcResult = requestCreateErrand(title, content,payDivision,pay)
         .andReturn();
 
+    return parseNewId(mvcResult);
+  }
+
+  private Long parseNewId(MvcResult mvcResult) {
     String newErrandUri = mvcResult.getResponse().getHeader("Location");
     String newErrandId = newErrandUri.replace("/errands/", "");
     return Long.parseLong(newErrandId);
@@ -237,5 +299,25 @@ class ErrandIntegrationTest extends IntegrationTest {
     return mvc.perform(
             get("/errands/{id}", errandId)
                 .contentType(MediaType.APPLICATION_JSON));
+  }
+
+  private static MockMultipartFile makeMultipartFile() {
+    return new MockMultipartFile(
+        "images",
+        "hello.txt",
+        MediaType.TEXT_PLAIN_VALUE,
+        "Hello, World!".getBytes()
+    );
+  }
+
+  /**
+   * 리스트에 담긴 파일이름에 해당하는 파일을 삭체처리합니다.
+   * @param fileNames
+   * @throws Exception
+   */
+  public void removeFilesAfterTest(List<String> fileNames) throws Exception {
+    for(String fileName: fileNames) {
+      mvc.perform(delete("/images/{filename}", fileName));
+    }
   }
 }
