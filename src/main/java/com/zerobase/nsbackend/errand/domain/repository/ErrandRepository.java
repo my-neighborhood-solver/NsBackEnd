d7cbd9ea1948a29f5fb296960647b54fd82897ea
gyureal <yonggyu.jeong10@gmail.com>
2023-08-08 19:23:53 +0900

    fix(errand): 의뢰 조회 시 dto 조회, fetch join 추가 - 의뢰 조회 시, service에서 dto 반환하도록 수정 - 의뢰 조회 시, 해쉬태그, 사진 fetch join 추가
    related: #66

--- src/main/java/com/zerobase/nsbackend/errand/domain/repository/ErrandRepository.java
+++ src/main/java/com/zerobase/nsbackend/errand/domain/repository/ErrandRepository.java
@@ -1,8 +1,17 @@
 package com.zerobase.nsbackend.errand.domain.repository;
 
 import com.zerobase.nsbackend.errand.domain.entity.Errand;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ErrandRepository extends JpaRepository<Errand, Long> {
-
+  @Query("select e "
+      + "from Errand e"
+      + " left join fetch e.images "
+      + " left join fetch e.hashtags "
+      + " where e.id = :id")
+  Optional<Errand> findErrandWithImagesAndHashTagById(@Param("id") Long id);
 }
