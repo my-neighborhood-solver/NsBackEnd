d66469a1a82273cf6243c0c678ca595c3169f29a
gyureal <yonggyu.jeong10@gmail.com>
2023-08-13 17:57:31 +0900

    test(errand): 의뢰 검색 기능 test 추가 - 의뢰 검색 조건 없이 전체 검색 테스트 - 의뢰 제목 검색 조건 테스트
    #61

---
+++ src/test/java/com/zerobase/nsbackend/errand/domain/repository/ErrandQueryDslRepositoryTest.java
@@ -0,0 +1,96 @@
+package com.zerobase.nsbackend.errand.domain.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.zerobase.nsbackend.RepositoryTest;
+import com.zerobase.nsbackend.errand.domain.entity.Errand;
+import com.zerobase.nsbackend.errand.domain.entity.ErrandHashtag;
+import com.zerobase.nsbackend.errand.domain.entity.ErrandImage;
+import com.zerobase.nsbackend.errand.domain.vo.ErrandStatus;
+import com.zerobase.nsbackend.errand.domain.vo.PayDivision;
+import com.zerobase.nsbackend.errand.dto.ErrandSearchCondition;
+import com.zerobase.nsbackend.errand.dto.ErrandSearchResult;
+import com.zerobase.nsbackend.member.domain.Member;
+import java.util.HashSet;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@Rollback(value = false)
+class ErrandQueryDslRepositoryTest extends RepositoryTest {
+  @Autowired
+  TestEntityManager em;
+  @Autowired
+  JPAQueryFactory jpaQueryFactory;
+
+  ErrandQueryDslRepository errandQueryDslRepository;
+
+  Member member01;
+  Errand errand01;
+  @BeforeEach
+  void init() {
+    errandQueryDslRepository = new ErrandRepositoryImpl(jpaQueryFactory);
+     member01 = em.persistAndFlush(Member.builder()
+        .nickname("member01")
+        .password("pass")
+        .email("email")
+        .profileImage("/images")
+        .build());
+
+    errand01 = em.persistAndFlush(Errand.builder()
+        .errander(member01)
+        .title("첫번째 의뢰 입니다.")
+        .content("content")
+        .images(List.of(ErrandImage.of("images/01"), ErrandImage.of("images/02")))
+        .hashtags(new HashSet<>(List.of(ErrandHashtag.of("청소"), ErrandHashtag.of("알바"))))
+        .pay(10000)
+        .payDivision(PayDivision.HOURLY)
+        .viewCount(1)
+        .status(ErrandStatus.REQUEST)
+        .build());
+
+    errand01 = em.persistAndFlush(Errand.builder()
+        .errander(member01)
+        .title("두번째 의뢰 입니다.")
+        .content("content")
+        .images(List.of(ErrandImage.of("images/01"), ErrandImage.of("images/02")))
+        .hashtags(new HashSet<>(List.of(ErrandHashtag.of("청소"), ErrandHashtag.of("알바"))))
+        .pay(10000)
+        .payDivision(PayDivision.HOURLY)
+        .viewCount(1)
+        .status(ErrandStatus.REQUEST)
+        .build());
+
+    em.getEntityManager().flush();
+    em.getEntityManager().clear();
+  }
+
+  @Test
+  @DisplayName("검색 조건 없는 의뢰 검색(search)에 성공합니다.")
+  void search_with_no_condition_success() {
+    ErrandSearchCondition cond = ErrandSearchCondition.builder()
+        .build();
+
+    List<ErrandSearchResult> searchResult = errandQueryDslRepository.search(cond);
+
+    assertThat(searchResult).hasSize(2);
+  }
+
+  @Test
+  @DisplayName("의뢰 제목으로 의뢰 검색(search)에 성공합니다.")
+  void search_with_errand_title_success() {
+    ErrandSearchCondition cond = ErrandSearchCondition.builder()
+        .errandTitle("첫번째")
+        .build();
+
+    List<ErrandSearchResult> searchResult = errandQueryDslRepository.search(cond);
+
+    assertThat(searchResult).hasSize(1);
+  }
+}
