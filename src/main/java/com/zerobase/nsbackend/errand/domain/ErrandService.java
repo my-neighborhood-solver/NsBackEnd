fac66cdeff22cc092df56b9a7dc0f254e9953139
gyureal <yonggyu.jeong10@gmail.com>
2023-08-13 18:54:15 +0900

    feat(errand): 의뢰 반환시, 사진 full path 반환하도록 수정

--- src/main/java/com/zerobase/nsbackend/errand/domain/ErrandService.java
+++ src/main/java/com/zerobase/nsbackend/errand/domain/ErrandService.java
@@ -29,7 +29,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
