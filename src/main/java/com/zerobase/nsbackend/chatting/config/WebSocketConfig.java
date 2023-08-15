20b454b74efde0fb62717cd7553897f3bf8015a5
KIM <wltn9169@naver.com>
2023-08-15 22:07:03 +0900

    setAllowedOrigins -> setAllowedOriginPatterns

--- src/main/java/com/zerobase/nsbackend/chatting/config/WebSocketConfig.java
+++ src/main/java/com/zerobase/nsbackend/chatting/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+    registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
   }
 }
 
